9a75f8cb9f6b135b1e9025da03393012be5cfc82
ahmed bahrouni <61459785+ahmedebahrouni@users.noreply.github.com>
2022-02-27 00:02:09 +0100

    crud+métiers
    crud+3métier (tri,recherche,login)

---
+++ Gamingland/src/entity/User.java
@@ -0,0 +1,115 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entity;
+
+/**
+ *
+ * @author Asus
+ */
+public class User {
+    int   id_user; 
+    String    mdp,nom,prenom, email, role,adresse_user ; 
+    int numtel_user ;
+
+    public User() {
+    }
+
+    public User(int id_user, String mdp, String nom, String prenom, String email, String role,  int numtel_user,String adresse_user) {
+        this.id_user = id_user;
+        this.mdp = mdp;
+        this.nom = nom;
+        this.prenom = prenom;
+        this.email = email;
+        this.role = role;
+
+        
+        this.numtel_user = numtel_user;
+            this.adresse_user = adresse_user;
+    }
+
+   
+    
+    
+    
+    public int getId_user() {
+        return id_user;
+    }
+
+    public void setId_user(int id_user) {
+        this.id_user = id_user;
+    }
+
+    public String getMdp() {
+        return mdp;
+    }
+
+    public void setMdp(String mdp) {
+        this.mdp = mdp;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public String getAdresse_user() {
+        return adresse_user;
+    }
+
+    public void setAdresse_user(String adresse_user) {
+        this.adresse_user = adresse_user;
+    }
+
+    public int getNumtel_user() {
+        return numtel_user;
+    }
+
+    public void setNumtel_user(int numtel_user) {
+        this.numtel_user = numtel_user;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" + "id_user=" + id_user + ", mdp=" + mdp + ", nom=" + nom + ", prenom=" + prenom + ", email=" + email + ", role=" + role + ", adresse_user=" + adresse_user + ", numtel_user=" + numtel_user + '}';
+    }
+   
+
+
+
+
+
+
+
+
+
+}
