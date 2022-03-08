885ad7e3172dce832b529d9656fb09824b36c6ff
ahmed bahrouni <61459785+ahmedebahrouni@users.noreply.github.com>
2022-03-08 23:28:36 +0100

    final work for user :ajout des métiers et des apis
    api(sms,notification,pdf,excel+mailing) +métiers (gérer login +forget password+tri avnacé+modifier avancé)

---
+++ finaluser/src/utils/SmsApi.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package utils;
+
+import com.twilio.Twilio;
+import com.twilio.rest.api.v2010.account.Message;
+import com.twilio.type.PhoneNumber;
+
+/**
+ *
+ * @author Asus
+ */
+public class SmsApi {
+   public static final String ACCOUNT_SID="AC679c57d39b08669791aff42ccf065a4b";
+            public static final String AUTH_TOKEN="3a61c9d6f234c4ffc00e093f6ea455dd";
+
+    public  SmsApi (){
+         Twilio.init(ACCOUNT_SID, AUTH_TOKEN);
+    }
+    
+    public void  sendSMS ( String sendTo,String msgText){
+      
+          Message message= Message.creator(
+                  new PhoneNumber(sendTo),
+                  new PhoneNumber("+19107271734")
+                  , msgText).create();
+    
+}
+}
